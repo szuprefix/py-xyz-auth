a5173cd2107670e4f0ded042c824d8d6737499a1
denishuang <szuprefix@126.com>
2020-08-10 07:10:03 +0800

    object_id

--- xyz_auth/stats.py
+++ xyz_auth/stats.py
@@ -12,15 +12,15 @@ def stats_login(qset=None, measures=None, period=None):
     gm = {'login.wechat.mp.qrcode': u'电脑扫码', 'login.wechat.mp': u'微信公号', 'login.mobile': u'手机号', 'login': u'帐号密码',
           'login.temptoken': u'临时密码'}
     funcs = {
-        'today': lambda: dstat.stat("今天", count_field="object_id", distinct=True, only_first=True),
-        'yesterday': lambda: dstat.stat("昨天", count_field="object_id", distinct=True, only_first=True),
-        'all': lambda: qset.values("object_id").distinct().count(),
+        'today': lambda: dstat.stat("今天", count_field="owner_id", distinct=True, only_first=True),
+        'yesterday': lambda: dstat.stat("昨天", count_field="owner_id", distinct=True, only_first=True),
+        'all': lambda: qset.values("owner_id").distinct().count(),
         'count': lambda: dstat.get_period_query_set(period).count(),
-        'daily': lambda: dstat.stat(period, count_field='object_id', distinct=True),
+        'daily': lambda: dstat.stat(period, count_field='owner_id', distinct=True),
         'type': lambda: statutils.count_by(
             dstat.get_period_query_set(period),
             'name',
-            count_field='object_id',
+            count_field='owner_id',
             distinct=True, sort="-", group_map=gm)
     }
     return dict([(m, funcs[m]()) for m in measures])
