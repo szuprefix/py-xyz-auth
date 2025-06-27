e563b3b7a270fdc6608bec9d570112cee5d6e5d8
Denis Huang <szuprefix@126.com>
2025-06-27 22:50:28 +0800

    signal

--- xyz_auth/signals.py
+++ xyz_auth/signals.py
@@ -1,8 +1,8 @@
 from django.dispatch import Signal
 
-to_bind_user = Signal(providing_args=["old_user", "new_user"])
-to_get_user_profile = Signal(providing_args=["user", "request"])
-to_get_user_roles = Signal(providing_args=["user"])
-to_save_user_profile = Signal(providing_args=['user', 'profile'])
-to_get_role_model_map = Signal(providing_args=[])
-to_change_user_name = Signal(providing_args=["user", "name"])
+to_bind_user = Signal()    #providing_args=["old_user", "new_user"])
+to_get_user_profile = Signal()    #providing_args=["user", "request"])
+to_get_user_roles = Signal()    #providing_args=["user"])
+to_save_user_profile = Signal()    #providing_args=['user', 'profile'])
+to_get_role_model_map = Signal()    #providing_args=[])
+to_change_user_name = Signal()    #providing_args=["user", "name"])
